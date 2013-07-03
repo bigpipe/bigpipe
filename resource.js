02e5bf3ee4bbcb64f8041ece85354140cd226d83
3rd-Eden <info@3rd-Eden.com>
2013-07-03 13:16:48 +0200

    [minor] resrouce interface

---
+++ resource.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var Stream = require('stream');
+
+function Resource() {
+  if (!(this instanceof Resource)) return new Resource();
+  this.cache = {};
+}
+
+/**
+ * Invalidate the cache.
+ *
+ * @api public
+ */
+Resource.prototype.invalidate = function invalidate() {
+  this.cache = {};
+  return this;
+};
+
+//
+// Make the Resource extendable.
+//
+Resource.extend = require('extenable');
+
+//
+// Initialize.
+//
+module.exports = Resource;
