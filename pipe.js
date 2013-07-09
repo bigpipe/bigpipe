2a98054a782d524bf74bf3f8ee566d30be153880
3rd-Eden <info@3rd-Eden.com>
2013-07-09 23:32:31 +0200

    [minor] Pipe client constructor.

---
+++ pipe.js
@@ -0,0 +1,38 @@
+/*globals Primus */
+'use strict';
+
+/**
+ *
+ * @constructor
+ * @param {String} server The server address we need to connect to.
+ * @param {Object} options Pipe configuration
+ * @api public
+ */
+function Pipe(server, options) {
+  options = options || {};
+
+  this.stream = null;     // Reference to the connected Primus socket.
+  this.connect(server, options.primus);
+}
+
+/**
+ * A new Pagelet is flushed by the server. We should register it and update the
+ * content.
+ *
+ * @param {Object} pagelet Pagelet configuration.
+ * @api public
+ */
+Pipe.prototype.arrive = function arrive(pagelet) {
+  return this;
+};
+
+/**
+ * Setup a real-time connection to the pagelet server.
+ *
+ * @param {String} url The server address.
+ * @param {Object} options The primus configuration.
+ * @api private
+ */
+Pipe.prototype.connect = function connect(url, options) {
+  this.stream = new Primus(url, options);
+};
