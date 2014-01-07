d308f7240787f10164a871f99d670cd9ba2ea05c
3rd-Eden <info@3rd-Eden.com>
2014-01-07 19:43:21 +0100

    [major] Move the real-time code to it's own file.

--- index.js
+++ index.js
@@ -657,74 +657,7 @@ Pipe.readable('decorate', function decorate(req, res) {
  * @param {Spark} spark A real-time "socket".
  * @api private
  */
-Pipe.readable('connection', function connection(spark) {
-  //
-  // Setup the pipe substream which.
-  //
-  var orchestrate = spark.substream('pipe::orchestrate')
-    , pipe = this
-    , streams = {};
-
-  /**
-   * Configure a pagelet for substreaming.
-   *
-   * @param {Pagelet} pagelet The pagelet we need.
-   * @api private
-   */
-  function substream(pagelet) {
-    if (streams[pagelet.name]) return debug('already configured the Spark');
-
-    debug('creating a new substream for pagelet::%s (%s)', pagelet.name, pagelet.id);
-    var stream = streams[pagelet.name] = spark.substream('pagelet::'+ pagelet.name);
-
-    //
-    // Let the pagelet know that we've paired with a substream and spark.
-    //
-    if ('function' === typeof pagelet.pair) pagelet.pair(stream, spark);
-
-    //
-    // Incoming communication between the pagelet and it's substream.
-    //
-    stream.on('data', function substreamer(data) {
-      if (!pagelet) return debug('substream data event called after pagelet was removed');
-
-      switch (data.type) {
-        case 'rpc':
-          pagelet.trigger(data.method, data.args, data.id, stream);
-        break;
-      }
-    });
-
-    stream.on('end', function end() {
-      debug('substream has ended: %s/%s', pagelet.name, pagelet.id);
-      delete streams[pagelet.name];
-    });
-  }
-
-  //
-  // Incoming communication between our spark and the pagelet orchestration.
-  //
-  orchestrate.on('data', function orchestration(data) {
-    switch (data.type) {
-      case 'configure':
-        return;
-        var pagelet = pipe.expire.get(data.id);
-
-        if (pagelet) {
-          debug('registering Pagelet %s/%s as new substream', pagelet.name, data.id);
-          substream(pipe.expire.get(data.id));
-        }
-      break;
-    }
-  });
-
-  spark.on('end', function end() {
-    //
-    // Free all allocated pages and nuke all pagelets.
-    //
-    debug('connection has ended: %s were still active', Object.keys(streams));
-  });
-});
+Pipe.readable('connection', require('./primus'));
 
 /**
  * Register a new plugin.
