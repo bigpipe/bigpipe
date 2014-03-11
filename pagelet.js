8d8975daa39ac9dd146013bbd443ef3ec100c051
Swaagie <info@martijnswaagman.nl>
2014-03-11 10:54:12 +0100

    [fix] fail, saved pagelet.js changes to untracked file

--- pagelet.js
+++ pagelet.js
@@ -6,91 +6,80 @@
 var debug = require('debug')('bigpipe:pagelet')
   , Pagelet = require('pagelet');
 
-//
-// Plugin name.
-//
-//exports.name = 'test';
+/**
+ * Check if the given pagelet has been enabled for the page.
+ *
+ * @param {String} name The name of the pagelet.
+ * @api public
+ */
+Pagelet.writable('enabled', function enabled(name) {
+  return this.page.enabled.some(function some(pagelet) {
+    return pagelet.name === name;
+  });
+});
 
-//
-// Transform the Pagelet on the server side once, to add some helper
-// functions and allow configure to be called post construction.
-//
+/**
+ * Check if the given pagelet has been disabled for the page.
+ *
+ * @param {String} name The name of the pagelet.
+ * @api public
+ */
+Pagelet.writable('disabled', function disabled(name) {
+  return this.page.disabled.some(function some(pagelet) {
+    return pagelet.name === name;
+  });
+});
 
-    /**
-     * Check if the given pagelet has been enabled for the page.
-     *
-     * @param {String} name The name of the pagelet.
-     * @api public
-     */
-    Pagelet.writable('enabled', function enabled(name) {
-      return this.page.enabled.some(function some(pagelet) {
-        return pagelet.name === name;
-      });
-    });
+/**
+ * Get route parameters that we've extracted from the route.
+ *
+ * @type {Object}
+ * @public
+ */
+Pagelet.writable('params', {
+  enumerable: false,
+  get: function params() {
+    return this.page.params;
+  }
+}, true);
 
-    /**
-     * Check if the given pagelet has been disabled for the page.
-     *
-     * @param {String} name The name of the pagelet.
-     * @api public
-     */
-    Pagelet.writable('disabled', function disabled(name) {
-      return this.page.disabled.some(function some(pagelet) {
-        return pagelet.name === name;
-      });
-    });
+/**
+ * Renderer takes care of all the data merging and `render` invocation.
+ *
+ * @param {Function} fn Completion callback.
+ * @api private
+ */
+Pagelet.writable('renderer', function renderer(fn) {
+  var page = this.page
+    , pagelet = this;
 
-    /**
-     * Get route parameters that we've extracted from the route.
-     *
-     * @type {Object}
-     * @public
-     */
-    Pagelet.writable('params', {
-      enumerable: false,
-      get: function params() {
-        return this.page.params;
-      }
-    }, true);
+  this.render(function receive(err, data) {
+    if (err) debug('rendering %s/%s resulted in a error', pagelet.name, pagelet.id, err);
 
+    //
+    // If the response was closed, finished the async asap.
+    //
+    if (page.res.finished) {
+      return fn(new Error('Response was closed, unable to write Pagelet'));
+    }
 
-    /**
-     * Renderer takes care of all the data merging and `render` invocation.
-     *
-     * @param {Function} fn Completion callback.
-     * @api private
-     */
-    Pagelet.writable('renderer', function renderer(fn) {
-      var page = this.page
-        , pagelet = this;
+    page.write(pagelet, data, fn);
+  });
+});
 
-      this.render(function receive(err, data) {
-        if (err) debug('rendering %s/%s resulted in a error', pagelet.name, pagelet.id, err);
+/**
+ * Add references to the page and pipe instance.
+ *
+ * @param {Object} options
+ * @api private
+ */
+Pagelet.writable('configure', function configure(options) {
+  options = options || {};
 
-        //
-        // If the response was closed, finished the async asap.
-        //
-        if (page.res.finished) {
-          return fn(new Error('Response was closed, unable to write Pagelet'));
-        }
+  this.pipe = options.page.pipe || options.pipe;
+  this.page = options.page;
 
-        page.write(pagelet, data, fn);
-      });
-    });
-
-    /**
-     * Add references to the page and pipe instance.
-     *
-     * @param {Object} options
-     * @api private
-     */
-    Pagelet.writable('configure', function configure(options) {
-      options = options || {};
-
-      this.pipe = options.page.pipe || options.pipe;
-      this.page = options.page;
-
-      return this;
-    });
+  return this;
+});
 
 module.exports = Pagelet;
