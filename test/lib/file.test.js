a2a438790f456820bf983d9f13a0f91646c9a869
Swaagie <info@martijnswaagman.nl>
2014-04-03 22:27:59 +0200

    [test] patch against changes

--- test/lib/file.test.js
+++ test/lib/file.test.js
@@ -5,11 +5,13 @@ describe('File', function () {
     , expect = common.expect
     , File = common.File
     , location = './path/to/file.js'
+    , ext = '.js'
+    , sha = '34cc87dddd9bb2b2123a973830feea9af4c7d149'
     , code = 'tiny piece of code'
     , file;
 
   beforeEach(function () {
-    file = new File(location);
+    file = new File(code, ext, location);
   });
 
   afterEach(function () {
@@ -27,9 +29,15 @@ describe('File', function () {
   });
 
   it('stores references to provided data', function () {
-    expect(file).to.have.property('location', location);
-    expect(file).to.have.property('type', file.mime['.js']);
-    expect(file).to.have.property('extname', '.js');
+    expect(file).to.have.property('location', '/' + sha + ext);
+    expect(file).to.have.property('type', file.mime[ext]);
+    expect(file).to.have.property('extname', ext);
+    expect(file).to.have.property('hash', sha);
+    expect(file).to.have.property('length');
+    expect(file).to.have.property('code', code);
+    expect(file).to.have.property('buffer');
+    expect(file).to.have.property('aliases');
+    expect(file.aliases).to.include(location);
   });
 
   describe('#set', function () {
@@ -41,7 +49,7 @@ describe('File', function () {
     });
 
     it('will be called on construction if content is provided', function () {
-      var result = new File(location, 'custom');
+      var result = new File('custom', ext, location);
 
       expect(result.code).to.equal('custom');
       expect(result.buffer).to.be.an.instanceof(Buffer);
@@ -60,11 +68,11 @@ describe('File', function () {
   describe('#is', function () {
     it('is a function that returns a boolean', function () {
       expect(file.is).to.be.a('function');
-      expect(file.is('any')).to.be.an('boolean');
+      expect(file.is('any')).to.be.a('boolean');
     });
 
     it('returns true if the file is of provided type', function () {
-      expect(file.is('.js')).to.equal(true);
+      expect(file.is(ext)).to.equal(true);
     });
 
     it('returns false if the file is not of provided type', function () {
@@ -77,6 +85,55 @@ describe('File', function () {
   });
 
   describe('#encrypt', function () {
-    it('returns sha1 encrypted representation of file content');
+    it('is a function that returns a string', function () {
+      expect(file.encrypt).to.be.a('function');
+      expect(file.encrypt('any')).to.be.a('string');
+    });
+
+    it('returns sha1 encrypted representation of file content', function () {
+      expect(file.encrypt(code)).to.equal(sha);
+    });
+  });
+
+  describe('#get', function () {
+    it('is a function that returns a string', function () {
+      expect(file.get).to.be.a('function');
+      expect(file.get()).to.be.an('object');
+    });
+
+    it('returns buffer by default', function () {
+      expect(JSON.stringify(file.get())).to.include('116,105,110,121');
+      expect(file.get()).to.be.an.instanceof(Buffer);
+    });
+
+    it('returns code if readable flag is supplied', function () {
+      expect(file.get(true)).to.be.a('string');
+      expect(file.get(true)).to.equal(code);
+    });
+  });
+
+  describe('#append', function () {
+    it('is a function that returns a string', function () {
+      expect(file.append).to.be.a('function');
+      expect(file.append('any')).to.be.a('string');
+    });
+
+    it('returns content with selector attached', function () {
+      expect(file.append(code)).to.equal(code + '#pagelet_' + sha + ' { height: 42px }');
+    });
+
+    it('can be provided with alternative hash', function () {
+      expect(file.append(code, 'test')).to.equal(code + '#pagelet_test { height: 42px }');
+    });
+  });
+
+  describe('#location', function () {
+    it('is a getter that returns a string', function () {
+      expect(file.location).to.be.a('string');
+    });
+
+    it('returns the location', function () {
+      expect(file.location).to.equal('/' + sha + ext);
+    });
   });
 });
