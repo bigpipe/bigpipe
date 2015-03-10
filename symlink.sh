87e0d4ed9690642e88231d1ce30e68e1074cfc23
3rd-Eden <info@3rd-Eden.com>
2015-03-10 14:29:55 +0100

    [minor] rewrite all the symlink roots

--- symlink.sh
+++ symlink.sh
@@ -1,4 +1,11 @@
 #
+# Get the absolute location of this script so we can correctly symlink the files
+# and other dependencies can also force bigpipe in to submission by manually calling
+# this supplied symlink file.
+#
+ROOT="$( cd "$( dirname "$0" )" && pwd )"
+
+#
 # Tell npm to install our core dependencies through the github master branches.
 #
 npm install bigpipe/pagelet bigpipe/bigpipe.js bigpipe/bootstrap-pagelet bigpipe/500-pagelet bigpipe/404-pagelet bigpipe/diagnostics-pagelet
@@ -7,18 +14,18 @@ npm install bigpipe/pagelet bigpipe/bigpipe.js bigpipe/bootstrap-pagelet bigpipe
 # As a lot of dependencies referrer to pagelet's we need to correct their
 # dependencies so they all use the pagelet's master branch
 #
-rm -rf $PWD/node_modules/bootstrap-pagelet/node_modules/pagelet
-ln -s $PWD/node_modules/pagelet $PWD/node_modules/bootstrap-pagelet/node_modules
+rm -rf $ROOT/node_modules/bootstrap-pagelet/node_modules/pagelet
+ln -s $ROOT/node_modules/pagelet $ROOT/node_modules/bootstrap-pagelet/node_modules
 
-rm -rf $PWD/node_modules/diagnostics-pagelet/node_modules/pagelet
-ln -s $PWD/node_modules/pagelet $PWD/node_modules/diagnostics-pagelet/node_modules
+rm -rf $ROOT/node_modules/diagnostics-pagelet/node_modules/pagelet
+ln -s $ROOT/node_modules/pagelet $ROOT/node_modules/diagnostics-pagelet/node_modules
 
-rm -rf $PWD/node_modules/404-pagelet/node_modules/pagelet
-rm -rf $PWD/node_modules/404-pagelet/node_modules/diagnostics-pagelet
-ln -s $PWD/node_modules/pagelet $PWD/node_modules/404-pagelet/node_modules
-ln -s $PWD/node_modules/diagnostics-pagelet $PWD/node_modules/404-pagelet/node_modules
+rm -rf $ROOT/node_modules/404-pagelet/node_modules/pagelet
+rm -rf $ROOT/node_modules/404-pagelet/node_modules/diagnostics-pagelet
+ln -s $ROOT/node_modules/pagelet $ROOT/node_modules/404-pagelet/node_modules
+ln -s $ROOT/node_modules/diagnostics-pagelet $ROOT/node_modules/404-pagelet/node_modules
 
-rm -rf $PWD/node_modules/500-pagelet/node_modules/pagelet
-rm -rf $PWD/node_modules/500-pagelet/node_modules/diagnostics-pagelet
-ln -s $PWD/node_modules/pagelet $PWD/node_modules/500-pagelet/node_modules
-ln -s $PWD/node_modules/diagnostics-pagelet $PWD/node_modules/500-pagelet/node_modules
+rm -rf $ROOT/node_modules/500-pagelet/node_modules/pagelet
+rm -rf $ROOT/node_modules/500-pagelet/node_modules/diagnostics-pagelet
+ln -s $ROOT/node_modules/pagelet $ROOT/node_modules/500-pagelet/node_modules
+ln -s $ROOT/node_modules/diagnostics-pagelet $ROOT/node_modules/500-pagelet/node_modules
